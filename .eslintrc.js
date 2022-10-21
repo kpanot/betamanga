5ecb1107ecd428e7661fbdd7379674e397a1ae57
Kilian Panot <kilian.panot@amadeus.com>
2022-10-21 20:20:17 +0200

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ['standard-with-typescript', 'plugin:prettier/recommended'],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    '@typescript-eslint/strict-boolean-expressions': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/return-await': 'off',
+    '@typescript-eslint/array-type': [
+      'error',
+      {
+        default: 'array',
+      },
+    ],
+    '@typescript-eslint/explicit-member-accessibility': [
+      'error',
+      {
+        accessibility: 'explicit',
+        overrides: {
+          accessors: 'explicit',
+          constructors: 'no-public',
+          methods: 'explicit',
+          properties: 'off',
+          parameterProperties: 'explicit',
+        },
+      },
+    ],
+  },
+};
---
+++ README.md
@@ -0,0 +1,22 @@
+# BetaManga
+
+Application to follow manga/book read
+
+## Pre-requisites
+
+- Docker
+- Node 16+
+
+## How to start
+
+To start in dev-mode you need to build the Docker:
+
+```shell
+yarn docker:dev:build
+```
+
+Then you can run locally the application with:
+
+```shell
+yarn docker:dev:build
+```
